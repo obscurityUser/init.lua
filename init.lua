93a432ac6354c137dc9bad8559a49837b8d11d7f
obscurityUser <145868864+obscurityUser@users.noreply.github.com>
2023-10-14 16:58:37 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -71,7 +71,6 @@ require("lazy").setup({
   -- Git related plugins
   "tpope/vim-fugitive",
   "tpope/vim-rhubarb",
-  "ThePrimeagen/harpoon",
   "mbbill/undotree",
   -- formatter.nvim
   {
@@ -317,26 +316,6 @@ vim.o.completeopt = "menuone,noselect"
 
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
-
-
--- [[ Basic Keymaps ]]
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
-
-vim.keymap.set("n", "<leader>ha", mark.add_file, { desc = "[A][d]d file to harpoon" })
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-vim.keymap.set("n", "<C-h>", function()
-  ui.nav_file(1)
-end)
-vim.keymap.set("n", "<C-t>", function()
-  ui.nav_file(2)
-end)
-vim.keymap.set("n", "<C-n>", function()
-  ui.nav_file(4)
-end)
-vim.keymap.set("n", "<C-s>", function()
-  ui.nav_file(4)
-end)
 
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open netrw" })
